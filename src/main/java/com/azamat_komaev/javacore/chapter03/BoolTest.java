a47a7f650cbc67752ecbfac6b81467af856de661
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-25 21:12:46 +0300

    add BoolTest.java with using boolean type

---
+++ src/main/java/com/azamat_komaev/javacore/chapter03/BoolTest.java
@@ -0,0 +1,19 @@
+// Применить тип данных boolean
+class BoolTest {
+    public static void main(String args[]) {
+        boolean b;
+
+        b = false;
+        System.out.println("b равно " + b);
+        b = true;
+        System.out.println("b равно " + b);
+
+        // значение типа boolean можно управлять оператором if
+        if (b) System.out.println("Этот код выполняется.");
+        b = false;
+        if (b) System.out.println("Этот код не выполняется.");
+
+        // результат сравнения - значение типа boolean
+        System.out.println("10 > 9 равно " + (10 > 9));
+    }
+}
