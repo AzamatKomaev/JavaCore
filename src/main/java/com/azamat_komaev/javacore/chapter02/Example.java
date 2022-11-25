a942229a7d98acb342687d1539a740a79ada64fd
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-25 15:40:27 +0300

    add Example.java with simple java application

---
+++ src/main/java/com/azamat_komaev/javacore/chapter02/Example.java
@@ -0,0 +1,6 @@
+class Example {
+    // This program start with calling main() method
+    public static void main(String args[]) {
+        System.out.println("Простая программа на Java.");
+    }
+}
