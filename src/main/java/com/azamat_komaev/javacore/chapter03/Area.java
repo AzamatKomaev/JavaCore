85c484a78ba11f84bfcca58dbf154881363a66aa
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-25 20:55:25 +0300

    add Area.java with getting area of circle

---
+++ src/main/java/com/azamat_komaev/javacore/chapter03/Area.java
@@ -0,0 +1,11 @@
+// Вычислить площадь круга
+class Area {
+    public static void main(String args[]) {
+        double pi, r, a;
+        r = 10.8;       // радиус окружности
+        pi = 3.1416;    // значение числа pi
+        a = pi * r * r; // вычислить площадь круга
+
+        System.out.println("Площадь круга равна " + a);
+    }
+}
