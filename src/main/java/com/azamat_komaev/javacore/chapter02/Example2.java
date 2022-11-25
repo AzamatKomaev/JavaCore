c949d3ab551af8da77f8973e49be263bf742aee8
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-25 16:05:04 +0300

    add Example2.java

---
+++ src/main/java/com/azamat_komaev/javacore/chapter02/Example2.java
@@ -0,0 +1,16 @@
+/*
+  Это еще один короткий пример программы.
+*/
+class Example2 {
+    public static void main(String args[]) {
+        int num; // объявляем переменную num
+        num = 100; // присваиваем переменной num значение 100
+
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
