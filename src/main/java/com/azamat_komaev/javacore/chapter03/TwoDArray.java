480dc6d494d7ee320b2083041dfaf16151a2d967
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-26 16:03:14 +0300

    add TwoDArray.java with creating 2d array

---
+++ src/main/java/com/azamat_komaev/javacore/chapter03/TwoDArray.java
@@ -0,0 +1,22 @@
+// Продемонстрировать применение двухмерного массива
+class TwoDArray {
+    public static void main(String args[]) {
+        int twoD[][] = new int[4][5];
+        int i, j, counter = 0;
+
+        for (i=0; i<4; i++) {
+            for (j=0; j<5; j++) {
+                twoD[i][j] = counter;
+                counter++;
+            }
+        }
+
+
+        for (i=0; i<4; i++) {
+            for (j=0; j<5; j++) {
+                System.out.print(twoD[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
