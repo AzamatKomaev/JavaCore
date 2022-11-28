679e50e8c6ff0d23f3c1effaca05bd3fd550d750
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-28 23:37:07 +0300

    add Continue.java with using continue operator

---
+++ src/main/java/com/azamat_komaev/javacore/chapter05/Continue.java
@@ -0,0 +1,14 @@
+package com.azamat_komaev.javacore.chapter05;
+
+// Use continue operator
+public class Continue {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            System.out.print(i + " ");
+            if (i%2 == 0) {
+                continue;
+            }
+            System.out.println("");
+        }
+    }
+}
