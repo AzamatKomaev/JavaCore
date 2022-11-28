a01f269f151417b118e7a61d2c86a67c6318859d
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-28 23:28:00 +0300

    add While.java with using while loop

---
+++ src/main/java/com/azamat_komaev/javacore/chapter05/While.java
@@ -0,0 +1,12 @@
+package com.azamat_komaev.javacore.chapter05;
+
+// Использование цикла while
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("такт " + n);
+            n--;
+        }
+    }
+}
