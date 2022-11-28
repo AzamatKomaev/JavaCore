8886d6d87014857b6a3914d1a9da159deb674ae0
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-28 23:30:28 +0300

    add ForTick.java with using for loop

---
+++ src/main/java/com/azamat_komaev/javacore/chapter05/ForTick.java
@@ -0,0 +1,10 @@
+package com.azamat_komaev.javacore.chapter05;
+
+// Use "for" loop
+public class ForTick {
+    public static void main(String[] args) {
+        for (int i=10; i>0; i--) {
+            System.out.println("такт " + i);
+        }
+    }
+}
