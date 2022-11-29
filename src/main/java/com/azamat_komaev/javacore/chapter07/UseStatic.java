21f93b366a483901c1097f4f2de9fbf26244445a
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-29 22:57:14 +0300

    add UseStatic with using static vars, methods and blocks

---
+++ src/main/java/com/azamat_komaev/javacore/chapter07/UseStatic.java
@@ -0,0 +1,17 @@
+package com.azamat_komaev.javacore.chapter07;
+
+public class UseStatic {
+    static int num1 = 3;
+    static int num2;
+
+    static void meth(int num3) {
+        System.out.println("num3 = " + num3);
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+
+    static {
+        System.out.println("Static block is initialized.");
+        meth(42);
+    }
+}
