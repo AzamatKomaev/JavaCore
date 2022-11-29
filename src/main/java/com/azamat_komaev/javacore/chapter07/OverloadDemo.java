2d62ed61fc8fa37355f8200c9c0d75f993410ecb
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-29 20:43:22 +0300

    finish chapter06 and start chapter07.
    Add Test class in PassOb.java with using object as param for class
    method

--- src/main/java/com/azamat_komaev/javacore/chapter07/OverloadDemo.java
+++ src/main/java/com/azamat_komaev/javacore/chapter07/OverloadDemo.java
@@ -1,4 +1,36 @@
 package com.azamat_komaev.javacore.chapter07;
 
-public class OverloadDemo {
+// Use method overload
+class Overload {
+    void test() {
+        System.out.println("There are no params");
+    }
+
+    void test(int num) {
+        System.out.println("num: " + num);
+    }
+
+    void test(int num1, int num2) {
+        System.out.println("num1 and num2: " + num1 + " " + num2);
+    }
+
+    double test(double num) {
+        System.out.println("double num: " + num);
+        return num * num;
+    }
 }
+
+public class OverloadDemo {
+    public static void main(String[] args) {
+        Overload ob = new Overload();
+        double result;
+
+        ob.test();
+        ob.test(10);
+        ob.test(10, 20);
+        result = ob.test(50.5);
+        System.out.println("The result of calling ob.test(50.5): " + result);
+    }
+}
+
+
