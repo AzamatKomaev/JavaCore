f4b007131c626257eff64db0b2ef87c68bab82ce
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-01 19:50:04 +0300

    add AnotherClient class with using interface as type for creating object

---
+++ src/main/java/com/azamat_komaev/javacore/chapter08/AnotherClient.java
@@ -0,0 +1,16 @@
+package com.azamat_komaev.javacore.chapter08;
+
+class AnotherClient implements Callback {
+    public void callback(int num) {
+        System.out.println("num*num = " + num*num);
+    }
+
+    public static void main(String[] args) {
+        Callback c = new Client();
+        AnotherClient ob = new AnotherClient();
+
+        c.callback(42);
+        c = ob; // now variable relates to AnotherClient object
+        c.callback(42);
+    }
+}
