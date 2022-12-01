6240dd0fbfca3da31e09f8a3410f5f250cf0575a
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-01 19:33:01 +0300

    add Client class and Callback interface: first interface

---
+++ src/main/java/com/azamat_komaev/javacore/chapter08/Callback.java
@@ -0,0 +1,5 @@
+package com.azamat_komaev.javacore.chapter08;
+
+public interface Callback {
+    void callback(int param);
+}
---
+++ src/main/java/com/azamat_komaev/javacore/chapter08/Client.java
@@ -0,0 +1,17 @@
+package com.azamat_komaev.javacore.chapter08;
+
+public class Client implements Callback {
+    public void callback(int num) {
+        System.out.println("callback() with value " + num);
+    }
+
+    void nonInterfaceMethod() {
+        System.out.println("Non interface method!");
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.callback(20);
+        client.nonInterfaceMethod();
+    }
+}
