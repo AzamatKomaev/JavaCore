0cbc47d8cbaf154b8540143805381b5f1d50d2ff
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-02 17:00:45 +0300

    modify IntStack interface: add default method clear()

--- src/main/java/com/azamat_komaev/javacore/chapter09/stack/IntStack.java
+++ src/main/java/com/azamat_komaev/javacore/chapter09/stack/IntStack.java
@@ -4,4 +4,8 @@ package com.azamat_komaev.javacore.chapter09.stack;
 public interface IntStack {
     void push(int item);
     int pop();
+
+    default void clear() {
+        System.out.println("Method clear() is not supported");
+    }
 }
