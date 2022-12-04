5d4b9e07abdb27b32dab1fd87c209a67e15f5f1b
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-04 20:19:51 +0300

    add HashSetDemo: use HashSet

---
+++ src/main/java/com/azamat_komaev/javacore/chapter18/HashSetDemo.java
@@ -0,0 +1,17 @@
+package com.azamat_komaev.javacore.chapter18;
+
+import java.util.*;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<String>();
+
+        hs.add("A");
+        hs.add("B");
+        hs.add("C");
+        hs.add("D");
+        hs.add("E");
+
+        System.out.println(hs);
+    }
+}
