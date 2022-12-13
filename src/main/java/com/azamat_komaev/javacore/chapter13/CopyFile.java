91f46ed88c01a00d49e5754ca4becb9b4296e902
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-13 13:41:20 +0300

    modify CopyFile: add auto closing files

--- src/main/java/com/azamat_komaev/javacore/chapter13/CopyFile.java
+++ src/main/java/com/azamat_komaev/javacore/chapter13/CopyFile.java
@@ -7,18 +7,14 @@ import java.io.IOException;
 public class CopyFile {
     public static void main(String[] args) throws IOException {
         int content;
-        FileInputStream fileIn = null;
-        FileOutputStream fileOut = null;
 
         if (args.length != 2) {
             System.out.println("You have to enter two args!");
             return;
         }
 
-        try {
-            fileIn = new FileInputStream(args[0]);
-            fileOut = new FileOutputStream(args[1]);
-
+        try (FileInputStream fileIn = new FileInputStream(args[0]);
+             FileOutputStream fileOut = new FileOutputStream(args[1]);) {
             do {
                 content = fileIn.read();
                 if (content != -1) {
@@ -27,17 +23,6 @@ public class CopyFile {
             } while (content != -1);
         } catch (IOException e) {
             System.out.println("Error during in-out");
-        } finally {
-            try {
-                if (fileIn != null) fileIn.close();
-            } catch (IOException e2) {
-                System.out.println("Error during closing fileIn");
-            }
-            try {
-                if (fileOut != null) fileOut.close();
-            } catch (IOException e2) {
-                System.out.println("Error during closing fileOut");
-            }
         }
     }
 }
