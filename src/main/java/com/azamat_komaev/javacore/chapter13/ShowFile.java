c75c44c4134281be81a8169ba06c190c084aa56b
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-13 12:54:22 +0300

    modify ShowFile: make one try{} with two catch instead of three try{}

--- src/main/java/com/azamat_komaev/javacore/chapter13/ShowFile.java
+++ src/main/java/com/azamat_komaev/javacore/chapter13/ShowFile.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class ShowFile {
     public static void main(String[] args) {
         int symbol;
-        FileInputStream fileInput;
+        FileInputStream fileInput = null;
 
         if (args.length != 1) {
             throw new IllegalArgumentException("You have to enter file name in args!");
@@ -13,26 +13,25 @@ public class ShowFile {
 
         try {
             fileInput = new FileInputStream(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            return;
-        }
-
-        try {
             do {
                 symbol = fileInput.read();
                 if (symbol != -1) {
                     System.out.println((char) symbol);
                 }
             } while (symbol != -1);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return;
         } catch (IOException e) {
             System.out.println("Cannot read data from file!");
-        }
-
-        try {
-            fileInput.close();
-        } catch (IOException e) {
-            System.out.println("Cannot close file!");
+        } finally {
+            try {
+                if (fileInput != null) {
+                    fileInput.close()
+                };
+            } catch (IOException e) {
+                System.out.println("Cannot close file!");
+            }
         }
     }
 }
